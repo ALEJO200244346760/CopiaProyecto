1e2f95c3b7d143185fe9434d4c39dbaa61ce4d42
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-06-27 17:37:48 -0300

    actualizado

--- Backend/src/main/java/com/easyscore/config/SecurityConfig.java
+++ Backend/src/main/java/com/easyscore/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.easyscore.config;
 
 import com.easyscore.jwt.config.JwtAuthenticationEntryPoint;
 import com.easyscore.jwt.config.JwtRequestFilter;
-import com.easyscore.service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,20 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
 
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
-
-    @Autowired
-    private CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -52,11 +48,6 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")
-                        .userInfoEndpoint()
-                        .userService(customOAuth2UserService)
                 );
 
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
